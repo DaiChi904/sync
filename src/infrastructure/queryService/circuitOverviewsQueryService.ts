f210c788a80339186eb6927c0cc6f3b0e2614852
Daichi Uematsu <minimi.904.horo@gmail.com>
2025-07-19 01:51:20 +0900

    replace localStorage with circuitRepository

--- src/infrastructure/queryService/circuitOverviewsQueryService.ts
+++ src/infrastructure/queryService/circuitOverviewsQueryService.ts
@@ -3,26 +3,21 @@ import type {
   CircuitOverviewsQueryServiceGetAllOutput,
   ICircuitOverviewsQueryService,
 } from "@/domain/model/queryService/ICircuitOverviewsQueryService";
-import { CircuitDescription } from "@/domain/model/valueObject/circuitDescription";
-import { CircuitId } from "@/domain/model/valueObject/circuitId";
-import { CircuitTitle } from "@/domain/model/valueObject/circuitTitle";
-import { CreatedDateTime } from "@/domain/model/valueObject/createdDateTime";
-import { UpdatedDateTime } from "@/domain/model/valueObject/updatedDateTime";
-import type { ILocalStorage } from "../storage/localStorage";
+import type { ICircuitRepository } from "@/domain/model/repository/ICircuitRepository";
 
 interface CircuitOverviewQueryServiceDependencies {
-  localStorage: Pick<ILocalStorage<"circuit">, "get">;
+  circuitRepository: ICircuitRepository;
 }
 
 export class CircuitOverviewsQueryService implements ICircuitOverviewsQueryService {
-  private readonly localStorage: Pick<ILocalStorage<"circuit">, "get">;
+  private readonly circuitRepository: ICircuitRepository;
 
-  constructor({ localStorage }: CircuitOverviewQueryServiceDependencies) {
-    this.localStorage = localStorage;
+  constructor({ circuitRepository }: CircuitOverviewQueryServiceDependencies) {
+    this.circuitRepository = circuitRepository;
   }
 
   async getAll(): Promise<CircuitOverviewsQueryServiceGetAllOutput> {
-    const res = await this.localStorage.get();
+    const res = await this.circuitRepository.getAll();
 
     switch (res.ok) {
       case true: {
@@ -31,11 +26,11 @@ export class CircuitOverviewsQueryService implements ICircuitOverviewsQueryServi
         const circuitOverviews =
           rawCircuits?.map((circuit) =>
             CircuitOverview.from({
-              id: CircuitId.from(circuit.id),
-              title: CircuitTitle.from(circuit.title),
-              description: CircuitDescription.from(circuit.description),
-              createdAt: CreatedDateTime.fromString(circuit.createdAt),
-              updatedAt: UpdatedDateTime.fromString(circuit.updatedAt),
+              id: circuit.id,
+              title: circuit.title,
+              description: circuit.description,
+              createdAt: circuit.createdAt,
+              updatedAt: circuit.updatedAt,
             }),
           ) ?? [];
 

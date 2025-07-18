ecc08496436aecdfd1f7db9d8fbc873cc26ddc55
Daichi Uematsu <minimi.904.horo@gmail.com>
2025-07-19 01:47:03 +0900

    add new function to handle ModelValidationError

--- src/domain/model/modelValidationError.ts
+++ src/domain/model/modelValidationError.ts
@@ -4,3 +4,26 @@ export class ModelValidationError extends Error {
     this.name = "ModelValidationError";
   }
 }
+
+/**
+ * In this project, the principle is to handle errors with Result-typed value.
+ * But for ease of use, ModelValidationError might be thrown in the initialization of the model.
+ *
+ * To catch and handle this error, use this wrapper inside of handler layer.
+ * This method is not allowed to use outside of handler layer.
+ */
+export const handleValidationError = async (
+  successCallback: () => void | Promise<void>,
+  failureCallback: () => void | Promise<void>,
+): Promise<void> => {
+  try {
+    await Promise.resolve(successCallback());
+  } catch (err: unknown) {
+    if (err instanceof ModelValidationError) {
+      console.error(`Invalid value has been provided to the model. Detail: ${err.message}`);
+    } else {
+      console.error("Unknown error occurred:", err);
+    }
+    await Promise.resolve(failureCallback());
+  }
+};
--- src/handler/homePageHandler.ts
+++ src/handler/homePageHandler.ts
@@ -3,6 +3,7 @@
 import { useCallback, useEffect, useState } from "react";
 import type { CircuitOverview } from "@/domain/model/entity/circuitOverview";
 import { type HomePageError, homePageError, type IHomePageHandler } from "@/domain/model/handler/IHomePageHandler";
+import { handleValidationError } from "@/domain/model/modelValidationError";
 import type { IGetCircuitOverviewsUsecase } from "@/domain/model/usecase/IGetCircuitOverviewsUsecase";
 import { useObjectState } from "@/hooks/objectState";
 
@@ -15,19 +16,24 @@ export const HomePageHandler = ({ getCircuitOverviewsUsecase }: HomePageHandlerD
 
   const [circuitOverviews, setCircuitOverviews] = useState<Array<CircuitOverview> | undefined>(undefined);
 
-  const fetch = useCallback(async (): Promise<void> => {
-    const res = await getCircuitOverviewsUsecase.getOverviews();
+  const fetch = useCallback((): void => {
+    handleValidationError(
+      async () => {
+        const res = await getCircuitOverviewsUsecase.getOverviews();
 
-    switch (res.ok) {
-      case true: {
-        setCircuitOverviews(res.value);
-        break;
-      }
-      case false: {
-        console.error(res.error);
-        setError("failedToGetCircuitOverviewsError", true);
-      }
-    }
+        switch (res.ok) {
+          case true: {
+            setCircuitOverviews(res.value);
+            break;
+          }
+          case false: {
+            console.error(res.error);
+            setError("failedToGetCircuitOverviewsError", true);
+          }
+        }
+      },
+      () => setError("failedToGetCircuitOverviewsError", true),
+    );
   }, [getCircuitOverviewsUsecase, setError]);
 
   useEffect(() => {

c69cff4bc12f4d851598e2c509cb9771fee27c63
daichi904 <minimi.904.horo@gmail.com>
2025-08-03 20:55:20 +0900

    refactor error handling of circuitDetailQueryService

--- src/infrastructure/queryService/circuitDetailQueryService.ts
+++ src/infrastructure/queryService/circuitDetailQueryService.ts
@@ -4,16 +4,10 @@ import type {
 } from "@/domain/model/queryService/ICircuitDetailQueryService";
 import type { ICircuitRepository } from "@/domain/model/repository/ICircuitRepository";
 import type { CircuitId } from "@/domain/model/valueObject/circuitId";
+import { Attempt } from "@/utils/attempt";
 
 interface CircuitDetailQueryServiceDependencies {
   circuitRepository: ICircuitRepository;
-}
-
-export class CircuitDetailQueryServiceGetByIdError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "CircuitDetailQueryServiceGetByIdError";
-  }
 }
 
 export class CircuitDetailQueryService implements ICircuitDetailQueryService {
@@ -24,14 +18,24 @@ export class CircuitDetailQueryService implements ICircuitDetailQueryService {
   }
 
   async getById(id: CircuitId): Promise<CircuitDetailQueryServiceGetByIdOutput> {
-    const res = await this.circuitRepository.getById(id);
-    if (!res.ok) {
-      return { ok: false, error: new CircuitDetailQueryServiceGetByIdError("Circuit not found.") };
-    }
+    return await Attempt.asyncProceed(
+      async () => {
+        const res = await this.circuitRepository.getById(id);
+        if (!res.ok) {
+          throw new Attempt.Abort(`Failed to get circuit. Id: ${id}`, { cause: res.error });
+        }
 
-    return {
-      ok: true,
-      value: res.value,
-    };
+        return {
+          ok: true,
+          value: res.value,
+        } as const;
+      },
+      (err: unknown) => {
+        return {
+          ok: false,
+          error: err,
+        } as const;
+      },
+    );
   }
 }

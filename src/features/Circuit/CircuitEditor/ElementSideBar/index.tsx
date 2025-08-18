ddd7b0d5396054cc96aa72ae61657762154bfbd7
Daichi Uematsu <minimi.904.horo@gmail.com>
2025-08-18 13:07:03 +0900

    add missing input and output pins

--- src/features/Circuit/CircuitEditor/ElementSideBar/index.tsx
+++ src/features/Circuit/CircuitEditor/ElementSideBar/index.tsx
@@ -1,7 +1,7 @@
 import Flex from "@/components/atoms/Flex";
 import Typography from "@/components/atoms/Typography";
 import { CircuitNodeId } from "@/domain/model/valueObject/circuitNodeId";
-import type { CircuitNodePinId } from "@/domain/model/valueObject/circuitNodePinId";
+import { CircuitNodePinId } from "@/domain/model/valueObject/circuitNodePinId";
 import { CircuitNodeSize } from "@/domain/model/valueObject/circuitNodeSize";
 import { CircuitNodeType } from "@/domain/model/valueObject/circuitNodeType";
 import { Coordinate } from "@/domain/model/valueObject/coordinate";
@@ -18,19 +18,61 @@ interface ElementSideBarProps {
   }) => void;
 }
 
+type NodeType = "AND" | "ENTRY" | "EXIT" | "JUNCTION" | "OR" | "NOT";
+const nodes: Array<NodeType> = ["AND", "ENTRY", "EXIT", "JUNCTION", "OR", "NOT"];
+
 export default function ElementSideBar({ viewBox, addCircuitNode }: ElementSideBarProps) {
   const centerX = viewBox ? viewBox?.x + viewBox?.w / 2 : 0;
   const centerY = viewBox ? viewBox?.y + viewBox?.h / 2 : 0;
 
-  const add = (type: string) => {
-    addCircuitNode({
-      id: CircuitNodeId.generate(),
-      type: CircuitNodeType.from(type),
-      inputs: [],
-      outputs: [],
-      coordinate: Coordinate.from({ x: centerX, y: centerY }),
-      size: CircuitNodeSize.from({ x: 60, y: 40 }),
-    });
+  const add = (type: NodeType) => {
+    switch (type) {
+      case "AND":
+      case "OR": {
+        addCircuitNode({
+          id: CircuitNodeId.generate(),
+          type: CircuitNodeType.from(type),
+          inputs: [CircuitNodePinId.generate(), CircuitNodePinId.generate()],
+          outputs: [CircuitNodePinId.generate()],
+          coordinate: Coordinate.from({ x: centerX, y: centerY }),
+          size: CircuitNodeSize.from({ x: 60, y: 40 }),
+        });
+        break;
+      }
+      case "NOT":
+      case "JUNCTION": {
+        addCircuitNode({
+          id: CircuitNodeId.generate(),
+          type: CircuitNodeType.from(type),
+          inputs: [CircuitNodePinId.generate()],
+          outputs: [CircuitNodePinId.generate()],
+          coordinate: Coordinate.from({ x: centerX, y: centerY }),
+          size: CircuitNodeSize.from({ x: 60, y: 40 }),
+        });
+        break;
+      }
+      case "ENTRY": {
+        addCircuitNode({
+          id: CircuitNodeId.generate(),
+          type: CircuitNodeType.from(type),
+          inputs: [],
+          outputs: [CircuitNodePinId.generate()],
+          coordinate: Coordinate.from({ x: centerX, y: centerY }),
+          size: CircuitNodeSize.from({ x: 60, y: 40 }),
+        });
+        break;
+      }
+      case "EXIT": {
+        addCircuitNode({
+          id: CircuitNodeId.generate(),
+          type: CircuitNodeType.from(type),
+          inputs: [CircuitNodePinId.generate()],
+          outputs: [],
+          coordinate: Coordinate.from({ x: centerX, y: centerY }),
+          size: CircuitNodeSize.from({ x: 60, y: 40 }),
+        });
+      }
+    }
   };
 
   return (
@@ -42,7 +84,7 @@ export default function ElementSideBar({ viewBox, addCircuitNode }: ElementSideB
     >
       <Typography>Logic gates</Typography>
       <Flex wrap="wrap" style={{ marginTop: 10 }}>
-        {["AND", "ENTRY", "EXIT", "JUNCTION", "OR", "NOT"].map((type) => (
+        {nodes.map((type) => (
           <Flex
             key={type}
             className="animated"

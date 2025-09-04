7746114cd97be1358784bbd5e299d63db41b8ab9
Daichi Uematsu <minimi.904.horo@gmail.com>
2025-09-04 21:28:00 +0900

    refactor CircuitParserService

--- src/domain/model/service/ICircuitParserService.ts
+++ src/domain/model/service/ICircuitParserService.ts
@@ -3,7 +3,15 @@ import type { CircuitGraphData } from "../entity/circuitGraphData";
 import type { CircuitGuiData } from "../entity/circuitGuiData";
 import type { CircuitData } from "../valueObject/circuitData";
 
+export class CircuitParserServiceError extends Error {
+  constructor(message: string, options?: { cause?: unknown }) {
+    super(message);
+    this.name = "CircuitParserServiceError";
+    this.cause = options?.cause;
+  }
+}
+
 export interface ICircuitParserService {
-  parseToGuiData(circuitData: CircuitData): Result<CircuitGuiData>;
-  parseToGraphData(circuitData: CircuitData): Result<CircuitGraphData>;
+  parseToGuiData(circuitData: CircuitData): Result<CircuitGuiData, CircuitParserServiceError>;
+  parseToGraphData(circuitData: CircuitData): Result<CircuitGraphData, CircuitParserServiceError>;
 }

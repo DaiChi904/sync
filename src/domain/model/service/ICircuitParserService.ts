7f06a80c9f29f562dd0ecf09e347bf954eda14f3
daichi904 <minimi.904.horo@gmail.com>
2025-10-20 14:18:06 +0000

    Refactor error handling and interface of CircuitParserService

--- src/domain/model/service/ICircuitParserService.ts
+++ src/domain/model/service/ICircuitParserService.ts
@@ -1,17 +1,18 @@
 import type { Result } from "@/utils/result";
 import type { CircuitGraphData } from "../entity/circuitGraphData";
 import type { CircuitGuiData } from "../entity/circuitGuiData";
+import type { UnexpectedError } from "../unexpectedError";
 import type { CircuitData } from "../valueObject/circuitData";
 
-export class CircuitParserServiceError extends Error {
+export class CircuitDataParseError extends Error {
   constructor(message: string, options?: { cause?: unknown }) {
     super(message);
-    this.name = "CircuitParserServiceError";
+    this.name = "CircuitDataParseError";
     this.cause = options?.cause;
   }
 }
 
 export interface ICircuitParserService {
-  parseToGuiData(circuitData: CircuitData): Result<CircuitGuiData, CircuitParserServiceError>;
-  parseToGraphData(circuitData: CircuitData): Result<CircuitGraphData, CircuitParserServiceError>;
+  parseToGuiData(circuitData: CircuitData): Result<CircuitGuiData, CircuitDataParseError | UnexpectedError>;
+  parseToGraphData(circuitData: CircuitData): Result<CircuitGraphData, CircuitDataParseError | UnexpectedError>;
 }

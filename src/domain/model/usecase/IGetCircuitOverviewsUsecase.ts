734e2ae513507034dfb6dc9f4ec4cd455a4c854a
daichi904 <minimi.904.horo@gmail.com>
2025-10-21 05:31:24 +0000

    refactor GetCircuitOverviewsUsecase to use specific error types and remove custom error class

--- src/domain/model/usecase/IGetCircuitOverviewsUsecase.ts
+++ src/domain/model/usecase/IGetCircuitOverviewsUsecase.ts
@@ -1,14 +1,9 @@
 import type { Result } from "@/utils/result";
 import type { CircuitOverview } from "../entity/circuitOverview";
-
-export class GetCircuitOverviewsUsecaseError extends Error {
-  constructor(message: string, options?: { cause?: unknown }) {
-    super(message);
-    this.name = "GetCircuitOverviewsUsecaseError";
-    this.cause = options?.cause;
-  }
-}
+import type { DataIntegrityError } from "../infrastructure/dataIntegrityError";
+import type { InfraError } from "../infrastructure/infraError";
+import type { UnexpectedError } from "../unexpectedError";
 
 export interface IGetCircuitOverviewsUsecase {
-  getOverviews(): Promise<Result<Array<CircuitOverview>, GetCircuitOverviewsUsecaseError>>;
+  getOverviews(): Promise<Result<Array<CircuitOverview>, DataIntegrityError | InfraError | UnexpectedError>>;
 }
--- src/usecase/getCircuitOverviewsUsecase.ts
+++ src/usecase/getCircuitOverviewsUsecase.ts
@@ -1,9 +1,9 @@
 import type { CircuitOverview } from "@/domain/model/entity/circuitOverview";
+import { DataIntegrityError } from "@/domain/model/infrastructure/dataIntegrityError";
+import { InfraError } from "@/domain/model/infrastructure/infraError";
 import type { ICircuitOverviewsQueryService } from "@/domain/model/infrastructure/queryService/ICircuitOverviewsQueryService";
-import {
-  GetCircuitOverviewsUsecaseError,
-  type IGetCircuitOverviewsUsecase,
-} from "@/domain/model/usecase/IGetCircuitOverviewsUsecase";
+import { UnexpectedError } from "@/domain/model/unexpectedError";
+import type { IGetCircuitOverviewsUsecase } from "@/domain/model/usecase/IGetCircuitOverviewsUsecase";
 import type { Result } from "@/utils/result";
 
 interface GetCircuitOverviewsUsecaseDependencies {
@@ -17,28 +17,30 @@ export class GetCircuitOverviewsUsecase implements IGetCircuitOverviewsUsecase {
     this.circuitOverviewsQueryService = circuitOverviewsQueryService;
   }
 
-  async getOverviews(): Promise<Result<Array<CircuitOverview>, GetCircuitOverviewsUsecaseError>> {
+  async getOverviews(): Promise<Result<Array<CircuitOverview>, DataIntegrityError | InfraError | UnexpectedError>> {
     try {
       const res = await this.circuitOverviewsQueryService.getAll();
       if (!res.ok) {
-        throw new GetCircuitOverviewsUsecaseError("Failed to get circuit overviews.", {
-          cause: res.error,
-        });
+        throw res.error;
       }
 
       return { ok: true, value: res.value } as const;
     } catch (err: unknown) {
       console.error(err);
-      if (err instanceof GetCircuitOverviewsUsecaseError) {
-        return { ok: false, error: err };
+      switch (true) {
+        case err instanceof DataIntegrityError: {
+          const dataIntegrityError = err;
+          return { ok: false, error: dataIntegrityError };
+        }
+        case err instanceof InfraError: {
+          const infraError = err;
+          return { ok: false, error: infraError };
+        }
+        default: {
+          const unexpectedError = new UnexpectedError({ cause: err });
+          return { ok: false, error: unexpectedError };
+        }
       }
-
-      return {
-        ok: false,
-        error: new GetCircuitOverviewsUsecaseError("Unknown error occurred while getting circuit overviews.", {
-          cause: err,
-        }),
-      };
     }
   }
 }

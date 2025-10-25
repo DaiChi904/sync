69b817bb1a6da19eddbbed9423345f8f07ab5623
daichi904 <minimi.904.horo@gmail.com>
2025-10-25 09:25:40 +0000

    fix invalid component name

--- src/components/atoms/input/SecondaryInput.tsx
+++ src/components/atoms/input/SecondaryInput.tsx
@@ -4,6 +4,6 @@ interface SecondaryInputProps extends ComponentProps<"input"> {
   variant?: "filled" | "outlined";
 }
 
-export default function PrimaryInput({ variant = "outlined", ...props }: SecondaryInputProps) {
+export default function SecondaryInput({ variant = "outlined", ...props }: SecondaryInputProps) {
   return <input {...props} className={`input-secondary-${variant}`} />;
 }
